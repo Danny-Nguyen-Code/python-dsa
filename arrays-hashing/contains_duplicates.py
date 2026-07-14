fa5c6e47a4c2cada2064b7d0d517e328cdeaeded
Danny Nguyen <204911897+Danny-Nguyen-Code@users.noreply.github.com>
2026-07-13 17:18:51 -0700

    Solve contain duplicates problem
    Solve problem through brute force and through sorting.

---
+++ arrays-hashing/contains_duplicates.py
@@ -0,0 +1,23 @@
+def contains_duplicates(self, nums: List[int]) -> bool:
+    """
+    brute force:
+    Time: O(n^2)
+    Space: O(1)
+
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] == nums[j]: return True
+    return False
+    """
+
+    """
+    sort list in order, and duplicates will appear next to each other:
+    Time: O(n logn)
+    Space: O(1) (or O(n) depending on sortin algorithm)
+
+    nums.sort()
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i - 1]: return True
+    return False
+    """
+
