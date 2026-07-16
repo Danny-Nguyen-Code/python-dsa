fc64aafafb6e3048c72995502f0aabd8bcc5c74a
Danny Nguyen <204911897+Danny-Nguyen-Code@users.noreply.github.com>
2026-07-16 16:54:14 -0700

    Add hash map solution

--- arrays-hashing/anagram.py
+++ arrays-hashing/anagram.py
@@ -1,10 +1,28 @@
 def isAnagram(self, s: str, t: str) -> bool:
     """
     If len is diff, can't be anagram. Compare sorted strings
-    if len(s) == len(t):
+    Time: O(n logn)
+    Space: O(1)
+
+    if len(s) != len(t):
         return False
     
     return sorted(s) == sorted(t)
     """
 
+    """
+    Count number of chars in hash map and if the 2 hash maps are equal then the
+    two strings are anagrams
+    Time: O(n)
+    Space: O(n)
+
+    if len(s) != len(t):
+        return False
     
+    countS, countT = {}, {}
+    for i in range(len(s)):
+        countS[s[i]] = 1 + countS.get(s[i], 0)
+        countT[t[i]] = 1 + countT.get(t[i], 0)
+    return countS == countT
+    """
+
