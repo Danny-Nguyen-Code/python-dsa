5d56a95281b2eda56e472295d7451be3526a5f45
Danny Nguyen <204911897+Danny-Nguyen-Code@users.noreply.github.com>
2026-07-16 17:01:23 -0700

    Add hash map array solution
    Use single hash map array and increment and decrement according to each string.

--- arrays-hashing/anagram.py
+++ arrays-hashing/anagram.py
@@ -14,7 +14,7 @@ def isAnagram(self, s: str, t: str) -> bool:
     Count number of chars in hash map and if the 2 hash maps are equal then the
     two strings are anagrams
     Time: O(n)
-    Space: O(n)
+    Space: O(1) because we can only have at most 26 characters
 
     if len(s) != len(t):
         return False
@@ -26,3 +26,25 @@ def isAnagram(self, s: str, t: str) -> bool:
     return countS == countT
     """
 
+    """
+    Create hash map array of all letters, increment and decrement for 1st and
+    2nd string. Go through the array at the end and if any value is != 0, return
+    False
+    Time: O(n)
+    Space: O(1) because can only have 26 chars
+    
+    if len(s) != len(t):
+        return False
+
+    count = [0] * 26
+    for i in range(len(s)):
+        count[ord(s[i]) - ord('a')] += 1
+        count[ord(t[i]) - ord('a')] -= 1
+    
+    for val in count:
+        if val != 0:
+            return False
+    return True
+    """
+
+isAnagram("burrito", "rrutiob")
