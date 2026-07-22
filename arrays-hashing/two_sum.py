5d7a84066a2259a7f8ea46247f4009ca04a9ea4e
Danny Nguyen <204911897+Danny-Nguyen-Code@users.noreply.github.com>
2026-07-21 18:52:05 -0700

    Add brute force two_sum solution.

---
+++ arrays-hashing/two_sum.py
@@ -0,0 +1,12 @@
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    """
+    brute force solution
+    Time: O(n^2)
+    Space: O(1)
+    
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] +nums[j] == target:
+                return [i, j]
+    return []
+    """
