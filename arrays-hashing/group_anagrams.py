ffe6e752cd732995866f888cdd5fd85e114b4657
Danny Nguyen <204911897+Danny-Nguyen-Code@users.noreply.github.com>
2026-07-22 12:46:24 -0700

    Add hash map solution.

--- arrays-hashing/group_anagrams.py
+++ arrays-hashing/group_anagrams.py
@@ -11,8 +11,22 @@ def groupAanagrams(self, strs:List[str]) ->List[List[str]]:
     return list(res.values())
     """
 
+    """
+    Hash table
+    Time: O(n^2)
+    Space: O(n^2)
+
+    res = defaultdict(list)
+    for s in strs:
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+        res[tuple(count)].append(s)
+    return list(res.values())
+    """
+
 def main():
     # groupAnagrams()
 
 if __name__ == "__main__":
-    main()
+    main() 
